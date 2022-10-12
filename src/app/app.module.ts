28cc437e38c27500dcd1521b10c1e3ce88f99df4
SafdarThaheem <104439594+SafdarThaheem@users.noreply.github.com>
2022-10-12 20:04:19 +0500

    addingLoginAuthentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,20 +12,32 @@ import { environment } from '../environments/environment';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+
+
+// Routes Configuration
+const appRoutes:Routes=[
+  {path:'',component:SignInComponent},
+  {path:'signUp',component:SignUpComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideDatabase(() => getDatabase())
+    provideDatabase(() => getDatabase()),
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    provideAuth(() => getAuth())
 
   ],
   providers: [],
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { signInWithEmailAndPassword } from "@angular/fire/auth";
+import { Database} from '@angular/fire/database';
+import { Auth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  myReactiveLoginForm!: FormGroup;
+
+  constructor(public auth: Auth , public database: Database, public router : Router) { }
 
   ngOnInit(): void {
+    this.myReactiveLoginForm = new FormGroup({
+      'username' : new FormControl(null,[Validators.required,Validators.email]),
+      'password' : new FormControl(null, [Validators.required, Validators.minLength(8)])
+    })
+  }
+
+  onLoginForm(email : string, password : string){
+    console.log(this.myReactiveLoginForm.value);
+
+    // const auth = getAuth();
+  signInWithEmailAndPassword(this.auth, email , password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+
+    alert("User signed in")
+    this.router.navigate(['/signUp'])
+    this.myReactiveLoginForm.reset();
+  })
+  .catch((error) => {
+    alert("something went wrong");
+  });
   }
 
 }
