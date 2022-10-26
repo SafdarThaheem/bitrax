ac5d66d40f251cd6390d6633a4302bbe239cd9e7
SafdarThaheem <104439594+SafdarThaheem@users.noreply.github.com>
2022-10-26 13:13:08 +0500

    get SignIn User Data on Console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,6 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'bitrax';
-  items: Observable<any[]> | undefined;
 
   constructor(){
   }
--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { signInWithEmailAndPassword, Auth} from "@angular/fire/auth";
+import { signInWithEmailAndPassword, Auth, user} from "@angular/fire/auth";
 import { Router } from '@angular/router';
-import {AngularFireDatabase, AngularFireList} from '@angular/fire/compat/database';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-
+import {AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
@@ -18,8 +17,7 @@ export class SignInComponent implements OnInit {
   constructor(
     public auth: Auth ,
     public router : Router,
-    public db : AngularFireDatabase , 
-    private afAuth: AngularFireAuth) {}
+    public db : AngularFireDatabase) {}
 
   ngOnInit(): void {
     this.myReactiveLoginForm = new FormGroup({
@@ -30,36 +28,44 @@ export class SignInComponent implements OnInit {
 
 
   onLoginForm(email : string, password : string){
-
-  signInWithEmailAndPassword(this.auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user.email;
-    alert("User signed in " + user)
-    this.fetchData()
-  
-    this.myReactiveLoginForm.reset();
-  })
-  .catch((error) => {
-    alert("something went wrong");
-  });
+    signInWithEmailAndPassword(this.auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user.uid;
+      alert("User signed in " + user)
+      // this.fetchData(user)
+      this.getUserRec(user)
+    
+      this.myReactiveLoginForm.reset();
+    }).catch((error) => {
+      alert("something went wrong");
+    });
   } 
 
-  public profileData : any;
-  uEmail : string ='' ;
-  id : any;
 
+//  userPro: Observable<any[]> | undefined; 
+//  fetchData(uUid: string){
+//     this.userPro = this.db.list("users/superAdmin/" + uUid ).valueChanges()
+//     let Subscription = this.userPro.subscribe(u => console.log(u))
+//     Subscription.unsubscribe();
+//   }
 
-  fetchData(){
-    this.afAuth.authState.subscribe(
-      (res)=>{
-        this.profileData = res?.uid;
-        console.log(this.profileData)
-      }
-    )
-    this.db.list('users/superAdmin/' + this.profileData).valueChanges().subscribe(details => {
+  ngOnDestroy() {
+  }
 
-   })
+  
+  getUserRec(userID : string){
+    this.db.database
+      .ref('users/superAdmin/')
+      .child(userID)
+      .once('value')
+      .then(snapshot => {
+        if(snapshot.exists()){
+          console.log(snapshot.val())
+        }
+      }).catch(error => {
+        console.log(error)
+      }) 
   }
 
 }
